433e4e366e0b69898469a91fadcd7cb460fb9c32
kbpsystem777 <koleen.bp@outlook.com>
2021-07-10 22:25:16 +0800

    modifying homepage and adding styles

---
+++ src/components/Logo.js
@@ -0,0 +1,15 @@
+import logo from "../images/vpass-logo.png"
+
+const Logo = () => {
+  return (
+    <div className="text-center">
+      <img
+        src={logo}
+        className="img-fluid text-center img-rounded logo"
+        alt="VPass logo"
+      />
+    </div>
+  )
+}
+
+export default Logo
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,20 +1,23 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import Logo from "../components/Logo"
 
 const HomePage = () => {
   return (
     <div>
-      <h1 className="text-center">VPass</h1>
-      <p className="text-center">
+      <Logo />
+      <h3 className="text-center">
         Making Vaccination Passport easier and digital.
-      </p>
-      <div className="container text-center homepage-padding">
+      </h3>
+      <div className="container text-center">
         <div className="btn-row">
           <Link to="/admin/login" className="btn-spacing">
             <button className="btn btn-primary btn-lg">Admin Login</button>
           </Link>
           <Link to="/lgu-registration" className="btn-spacing">
-            <button className="btn btn-primary btn-lg">LGU Registration</button>
+            <button className="btn btn-secondary btn-lg">
+              LGU Registration
+            </button>
           </Link>
         </div>
       </div>
